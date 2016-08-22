b7c80d062ac567b0b6363b47c658aacdc47c7023
aniolog <aniolog@gmail.com>
2016-08-22 05:18:49 -0430

    First

---
+++ Copa/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name copaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the copaApp
+ */
+angular.module('copaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
